b47a21be32632bcf61667268965ffa4cbd2835a4
yuugrid <152932000+yuugrid@users.noreply.github.com>
2024-02-29 06:54:29 +0900

    実装完了

---
+++ src/main/java/com/example/mapper/ItemMapper.java
@@ -0,0 +1,21 @@
+package com.example.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.entity.Item;
+
+
+@Mapper
+public interface ItemMapper {
+	public List<Item> findAll();
+	
+	public void insert(Item item);
+	
+	public Item findById(Integer id);
+	
+	public void update(Item item);
+	
+	public void deleteById(Integer id);
+}
